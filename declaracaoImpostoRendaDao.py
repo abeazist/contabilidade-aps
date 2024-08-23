9bbeff4e560156170d13f5bc2e48020b95be62c3
dannyy7 <163425282+dannyy7@users.noreply.github.com>
2024-08-23 11:02:36 -0300

    taaipai

--- declaracaoImpostoRendaDao.py
+++ declaracaoImpostoRendaDao.py
@@ -21,4 +21,27 @@ class DeclaracaoImpostoJuridica:
         self.lucros = lucros 
         self.prejuizos =prejuizos
         self.receitas = receitas
+        self.baseCalculo = None
+        self.valorInformado = valorInformado
+        self.pagamentosRealizados = None
+
+
+# VALIDACOES IMPOSTO DE RENDA JURIDICA
+
+    def setPagamentosRealizados(self, pagamentosRealizados)
+        if self.pagamentosRealizados > 0 and self.pagamentosRealizados == valorInformado
+
+            self.__pagamentosRealizados = pagamentosRealizados
+
+        else:
+            raise "Os pagamentos realizados não podem ser negativos e tambem devem ser iguais aos valores informados nas notas fiscais ou recibos"
+
+
+    def setBaseCalculo(self, baseCalculo)
+        if self.baseCalculo >= 0 and self.baseCalculo == baseCalculoEsperada
+
+        else:
+            raise "A base de calculos nao pode possuir um valor negativo e tambem devem ter o valor igual a base de calculo esperada"
+
+    def setAliquotas(self, aliquotas)
         
